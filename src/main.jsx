8ede8ed7637973a71ac5ca82031201490707086e
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2025-07-09 22:42:38 +0600

    initialize client-side application with React, Vite, and Tailwind CSS

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Router";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <div className="max-w-screen-xl p-2 md:p-0 mx-auto">
+      <RouterProvider router={router} />
+      {/* <Toaster></Toaster> */}
+    </div>
+  </StrictMode>
+);
